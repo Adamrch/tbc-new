0f21add4a080071abf9ce6792b066b19e9db3d62
Adrian Klingen <adrian.klingen@deptagency.com>
2026-03-16 15:11:58 +0100

    Add Morogrim encounter

---
+++ sim/encounters/serpent_shrine_cavern/morogrim_tidewalker_ai.go
@@ -0,0 +1,223 @@
+package serpentshrinecavern
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/wowsims/tbc/sim/core"
+	"github.com/wowsims/tbc/sim/core/proto"
+	"github.com/wowsims/tbc/sim/core/stats"
+)
+
+const morogrimMeleeDamageSpread = 0.413
+const morogrimTidewalkerID int32 = 21213
+
+func addMorogrimTidewalker(raidPrefix string) {
+	createMorogrimPreset(raidPrefix, 25, 5_691_000, 12_744)
+}
+
+func createMorogrimPreset(raidPrefix string, raidSize int32, bossHealth float64, bossMinBaseDamage float64) {
+	bossName := fmt.Sprintf("Morogrim Tidewalker %d", raidSize)
+
+	core.AddPresetTarget(&core.PresetTarget{
+		PathPrefix: raidPrefix,
+
+		Config: &proto.Target{
+			Id:        morogrimTidewalkerID,
+			Name:      bossName,
+			Level:     73,
+			MobType:   proto.MobType_MobTypeGiant,
+			TankIndex: 0,
+
+			Stats: stats.Stats{
+				stats.Health:      bossHealth,
+				stats.Armor:       7685,
+				stats.AttackPower: 320,
+			}.ToProtoArray(),
+
+			SpellSchool:   proto.SpellSchool_SpellSchoolPhysical,
+			SwingSpeed:    1.6,
+			MinBaseDamage: bossMinBaseDamage,
+			DamageSpread:  morogrimMeleeDamageSpread,
+
+			ParryHaste: true,
+		},
+
+		AI: makeMorogrimAI(),
+	})
+
+	core.AddPresetEncounter(bossName, []string{
+		raidPrefix + "/" + bossName,
+	})
+}
+
+func makeMorogrimAI() core.AIFactory {
+	return func() core.TargetAI {
+		return &MorogrimAI{}
+	}
+}
+
+type MorogrimAI struct {
+	// Unit references
+	Target     *core.Target
+	BossUnit   *core.Unit
+	MainTank   *core.Unit
+	OffTank    *core.Unit
+	ValidTanks []*core.Unit
+
+	// Spell + aura references
+	Thrash        *core.Spell
+	Earthquake    *core.Spell
+	TidalWave     *core.Spell
+	TidalWaveAura *core.Aura
+}
+
+func (ai *MorogrimAI) Initialize(target *core.Target, config *proto.Target) {
+	// Save unit references
+	ai.Target = target
+	ai.Target.AutoAttacks.MHConfig().ActionID.Tag = morogrimTidewalkerID
+
+	ai.BossUnit = &target.Unit
+
+	ai.MainTank = ai.BossUnit.CurrentTarget
+
+	ai.ValidTanks = core.FilterSlice([]*core.Unit{ai.MainTank, ai.OffTank}, func(unit *core.Unit) bool {
+		return unit != nil
+	})
+
+	// Register relevant spells and auras
+	ai.registerTidalWave()
+	ai.registerEarthquake()
+	ai.registerThrash()
+}
+
+func (ai *MorogrimAI) registerTidalWave() {
+	duration := time.Second * 15
+
+	ai.TidalWaveAura = ai.Target.RegisterAura(core.Aura{
+		Label:    "Tidal Wave",
+		ActionID: core.ActionID{SpellID: 37730},
+		Duration: duration,
+	}).AttachMultiplyMeleeSpeed(1.0 / 4)
+
+	rollTidalWaveCD := func(sim *core.Simulation) time.Duration {
+		return duration + core.DurationFromSeconds(sim.Roll(0, 45))
+	}
+
+	ai.TidalWave = ai.BossUnit.RegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 37730},
+		SpellSchool: core.SpellSchoolFrost,
+		ProcMask:    core.ProcMaskEmpty,
+		Flags:       core.SpellFlagBinary,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD:      core.BossGCD,
+				CastTime: time.Second * 2,
+			},
+
+			CD: core.Cooldown{
+				Timer:    ai.BossUnit.NewTimer(),
+				Duration: duration,
+			},
+
+			IgnoreHaste: true,
+		},
+
+		DamageMultiplier: 1,
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.CalcAndDealDamage(sim, target, sim.Roll(3938, 5062), spell.OutcomeMagicHit)
+			ai.TidalWaveAura.Activate(sim)
+			spell.CD.Set(sim.CurrentTime + rollTidalWaveCD(sim))
+		},
+	})
+
+	ai.BossUnit.RegisterResetEffect(func(sim *core.Simulation) {
+		ai.TidalWave.CD.Set(rollTidalWaveCD(sim))
+	})
+}
+
+func (ai *MorogrimAI) registerEarthquake() {
+	cooldown := time.Second * 45
+	rollEarthquakeCD := func(sim *core.Simulation) time.Duration {
+		return cooldown + core.DurationFromSeconds(sim.Roll(0, 15))
+	}
+
+	ai.Earthquake = ai.BossUnit.RegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 37764},
+		SpellSchool: core.SpellSchoolPhysical,
+		ProcMask:    core.ProcMaskSpellDamage,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: core.BossGCD,
+			},
+
+			CD: core.Cooldown{
+				Timer:    ai.BossUnit.NewTimer(),
+				Duration: cooldown,
+			},
+
+			IgnoreHaste: true,
+		},
+
+		DamageMultiplier: 1,
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.CalcAndDealDamage(sim, target, sim.Roll(4000, 4200), spell.OutcomeMagicHit)
+			ai.Earthquake.CD.Set(sim.CurrentTime + rollEarthquakeCD(sim))
+		},
+	})
+
+	ai.BossUnit.RegisterResetEffect(func(sim *core.Simulation) {
+		ai.Earthquake.CD.Set(rollEarthquakeCD(sim))
+	})
+}
+
+func (ai *MorogrimAI) registerThrash() {
+	var thrashSpell *core.Spell
+
+	procTrigger := ai.Target.MakeProcTriggerAura(core.ProcTrigger{
+		Name:               "Thrash Listener",
+		ProcChance:         0.5,
+		ICD:                time.Second * 3,
+		ProcMask:           core.ProcMaskMeleeWhiteHit,
+		TriggerImmediately: true,
+		Callback:           core.CallbackOnSpellHitDealt,
+
+		Handler: func(sim *core.Simulation, _ *core.Spell, result *core.SpellResult) {
+			thrashSpell.Cast(sim, result.Target)
+		},
+	})
+
+	procTrigger.ApplyOnInit(func(aura *core.Aura, sim *core.Simulation) {
+		config := *ai.Target.AutoAttacks.MHConfig()
+		config.ActionID = config.ActionID.WithTag(morogrimTidewalkerID + 18943)
+		config.Flags |= core.SpellFlagPassiveSpell
+		thrashSpell = ai.Target.GetOrRegisterSpell(config)
+	})
+}
+
+func (ai *MorogrimAI) Reset(sim *core.Simulation) {
+	// Randomize GCD and swing timings to prevent fake APL-Haste couplings.
+	ai.Target.Enable(sim)
+}
+
+func (ai *MorogrimAI) ExecuteCustomRotation(sim *core.Simulation) {
+	target := ai.Target.CurrentTarget
+	if target == nil {
+		// For individual non tank sims we still want abilities to work
+		target = &ai.Target.Env.Raid.Parties[0].Players[0].GetCharacter().Unit
+	}
+
+	if ai.TidalWave.CanCast(sim, target) {
+		ai.TidalWave.Cast(sim, target)
+	}
+
+	if ai.Earthquake.CanCast(sim, target) {
+		ai.Earthquake.Cast(sim, target)
+	}
+
+	ai.Target.ExtendGCDUntil(sim, sim.CurrentTime+core.BossGCD)
+}
